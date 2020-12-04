9fca5eee1a54569b8567cf4e3daa39c5eacc45a8
cosminbbx <virgacosmin@gmail.com>
2020-12-04 18:27:02 +0200

    refactor

--- text_analyzer.py
+++ text_analyzer.py
@@ -1,29 +1,19 @@
 import sys
 import FileManager, ErrorHandler, TextParser , OutputHelper
 
-# try:
-fileText = FileManager.getText(sys.argv[1])
+def main():
+    try:
+        fileText = FileManager.getText(sys.argv[1])
 
-information = TextParser.parse(fileText)
+        information = TextParser.parse(fileText)
 
-OutputHelper.format(information)
+        OutputHelper.format(information)
+    except IndexError:
+        ErrorHandler.handleError("IndexError")
+    except FileNotFoundError:
+        ErrorHandler.handleError("FileNotFoundError")
+    except:
+        ErrorHandler.handleError("GenericError")
 
-#print(output)
-# except IndexError:
-#     ErrorHandler.handleError("IndexError")
-# except FileNotFoundError:
-#     ErrorHandler.handleError("FileNotFoundError")
-# except:
-#     ErrorHandler.handleError("GenericError")
+main()
 
-# f = open(filePath, "r")
-
-#fileText = FileManager.getText(sys.argv[1])
-
-# for i in range(0,len(fileText)):
-#     if fileText[i] == "\n":
-#         print(True)
-
-#print(fileText)
-
-#print(path)
